be55a9f69e87df258d2fc8468dc1465b0fae44b7
Ara Tetsuya <aradeaa9@gmail.com>
2020-02-02 21:45:16 +0700

    Menambahkan route

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/login', function() {
-	return view('auth.login');
+Route::get('/login', function () {
+    return view('auth.login');
 });
 
-
-
-
-
-
-
-
+//Pelanggaran
+Route::get('/pelanggaran', 'PelanggaranController@index');
+Route::get('/pelanggaran/tambah', 'PelanggaranController@tambah');
+Route::post('/pelanggaran/store', 'PelanggaranController@store');
+Route::get('/pelanggaran/edit/{id}', 'PelanggaranController@edit');
+Route::put('/pelanggaran/update/{id}', 'PelanggaranController@update');
+Route::get('/pelanggaran/hapus/{id}', 'PelanggaranController@hapus');
