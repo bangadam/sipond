f8ed4f7033e6cbbac6db8d31d0c2cdbd8300116c
Aurellesia <aurellesia19@gmail.com>
2020-02-04 05:50:51 +0700

    Konfirmasi kembali

--- resources/views/perizinans/datatables_actions.blade.php
+++ resources/views/perizinans/datatables_actions.blade.php
@@ -1,11 +1,19 @@
-{!! Form::open(['route' => ['perizinans.destroy', $id_izin], 'method' => 'delete']) !!}
+
 <div class='btn-group'>
-    <a href="#" class='btn btn-success btn-xs'>
-        Konfirmasi
-    </a>
+    <!-- konfirmasi -->
+    {!! Form::open(['route' => ['perizinans.konfirmasi', $id_izin], 'method' => 'put']) !!}
+    {!! Form::button('Konfirmasi', [
+        'type' => 'submit',
+        'class' => 'btn btn-success btn-xs',
+        'onclick' => "return confirm('Apakah santri sudah kembali?')"
+    ]) !!}
+    {!! Form::close() !!}
+    
+    {!! Form::open(['route' => ['perizinans.destroy', $id_izin], 'method' => 'delete']) !!}
     <a href="{{ route('perizinans.show', $id_izin) }}" class='btn btn-default btn-xs'>
         <i class="la la-eye"></i>
     </a>
+
     <a href="{{ route('perizinans.edit', $id_izin) }}" class='btn btn-default btn-xs'>
         <i class="la la-edit"></i>
     </a>
@@ -14,5 +22,6 @@
         'class' => 'btn btn-danger btn-xs',
         'onclick' => "return confirm('Are you sure?')"
     ]) !!}
+    {!! Form::close() !!}
 </div>
-{!! Form::close() !!}
+
