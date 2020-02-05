0ddb60626c4b1304eb9d63d22121bcb09a5361d8
noorvicki <business.miftach@gmail.com>
2020-02-06 06:53:17 +0700

    menambahkan link keuangan pada sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,7 +26,7 @@
                 <i class="kt-menu__section-icon flaticon-more-v2"></i>
             </li>
             <li class="kt-menu__item " aria-haspopup="true">
-                <a  href="#!" class="kt-menu__link ">
+                <a  href="{{route('pembayarans.index')}}" class="kt-menu__link ">
                     <span class="kt-menu__link-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
                             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
